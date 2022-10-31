0f303e68dd319e2e432ec08485782724c9a310bd
Thomas Farias <thomasfarias19@gmail.com>
2022-10-30 23:34:45 -0300

    Finalizado estilização das transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
@@ -7,7 +8,32 @@ export function Transactions() {
       <Header />
       <Summary />
 
-      
+      <TransactionContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionContainer>
     </div>
   )
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const TransactionContainer = styled.main`
+  // Manter conteúdo das transações centralizado
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  // Linha da tabela vai ocupar o tamanho máximo disponível
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  /* margin-top: 1.5rem */
+
+  // Conteúdo dos itens da linha
+  td {
+    padding: 1.25rem 2rem;
+    background: ${props => props.theme['gray-700']};
+
+    // Arredondar borda do inicio e final da linha
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+// Declarando possíveis variações de estilização dos valores da linha
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome'
+}
+
+// Se valor for positivo a é cor verde, se negativo a é cor vermelha
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-300']};
+`;
